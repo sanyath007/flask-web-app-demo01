25da4510443d7ed5f6dd48488938109d4d651127
sanyath007 <sanyath007@gmail.com>
2021-06-21 15:44:45 +0700

    Get requests data of sign_up method and set flash messages of validation data and render to base templates

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,32 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password = request.form.get('password')
+        confirm = request.form.get('confirm_password')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(name) < 2:
+            flash('Name must be greater than 1 characters.', category='error')
+        elif password != confirm:
+            flash('Password and confirm password not match.', category='error')
+        elif len(password) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            flash('Account created!!', category='success')
+
     return render_template("signup.html")
 
 
